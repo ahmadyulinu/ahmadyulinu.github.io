514b2edc520a619ac09296cb98c098253c036f5a
ahmadyulinu <ahamdyulinu@gmail.com>
2021-05-21 05:47:11 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        cinzel: ["Cinzel", "serif"],
+        cormorant: [`Cormorant Garamond`, "serif"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
